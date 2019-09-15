c21090496dd539564fdb1e1f4240e71d0e7c633f
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 18:10:06 +0200

    Added POST function. Added CampModel to Camp mapping. Added validation for POST model.

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,16 @@ using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Cotrollers {
     [Route("api/[controller]")] //Handles <url>/api/camps
+    [ApiController]
     public class CampsController : ControllerBase {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper) {
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator) {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
         
         [HttpGet] //Specifies it is a get request
@@ -55,6 +59,34 @@ namespace CoreCodeCamp.Cotrollers {
             } catch (Exception) {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
+        }
+
+        public async Task<ActionResult<CampModel>> Post(CampModel model) {
+            try {
+                var tempModel = _repository.GetCampAsync(model.Moniker);
+
+                if (tempModel != null) {
+                    return BadRequest("Moniker in use!");
+                }
+
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new {moniker = model.Moniker });
+
+                if (string.IsNullOrWhiteSpace(location)) {
+                    return BadRequest("Could not use current moniker!");
+                }
+
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);
+
+                if (await _repository.SaveChangesAsync()) {
+                    return Created(location, _mapper.Map<CampModel>(camp));
+                }
+
+            } catch (Exception) {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
         }
     }
 }
